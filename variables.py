9cc924cf14a25c0db8befd8395aea6a3b37b0df2
Grellheist <grellheist@tutanota.com>
2022-11-30 16:52:40 -0300

    Updated

--- variables.py
+++
@@ -1,8 +0,0 @@
-#age = 1 #int
-#height = 180.5 #float
-#boolean = False #boolean
-#name = "My name" # string
-
-age, height, boolean, name = 1, 180.5, False, "My name"
-
-print(age, height, name, boolean)
