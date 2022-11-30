983eaa375113ca38c24b24b1987a77b16b308ad0
Grellheist <grellheist@tutanota.com>
2022-11-30 16:50:44 -0300

    teste

---
+++ variables.py
@@ -0,0 +1,8 @@
+#age = 1 #int
+#height = 180.5 #float
+#boolean = False #boolean
+#name = "My name" # string
+
+age, height, boolean, name = 1, 180.5, False, "My name"
+
+print(age, height, name, boolean)
