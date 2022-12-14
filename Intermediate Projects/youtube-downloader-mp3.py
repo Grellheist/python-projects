f85a7110b3e9876eb908ce9e5620bc92a2a5c8cc
Grellheist <grellheist@tutanota.com>
2022-12-14 11:06:36 -0300

    Updated

--- Intermediate Projects/youtube-downloader-mp3.py
+++ Intermediate Projects/youtube-downloader-mp3.py
@@ -1,10 +1,13 @@
 from pytube import YouTube
 from sys import argv
+import os
 
 link = argv[1]  # Gets the link which is the second position on the command 
 yt = YouTube(link) 
 print("Title: ", yt.title)
-yt_download = yt.streams.get_highest_resolution()  # Downloads best resolution by default
-yt_download.download()
-
+yt_download = yt.streams.filter(only_audio=True).first()  # Downloads best resolution by default
+out_file = yt_download.download()
+base, ext = os.path.splitext(out_file)
+new_file = base + '.mp3'
+os.rename(out_file, new_file)
 # Command should follow: python3 youtube_downloader "[video link]"
